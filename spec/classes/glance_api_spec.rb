ef4c261479b1f7b2a83ce515c654c19abf0ccae1
Matt Fischer <matt@mattfischer.com>
2015-01-30 18:57:13 -0700

    Add openstack tag to glance packages
    Change-Id: Ifbc41027c11f62342397d7e60e71080336c3418e
    Partial-Bug: #1391209

--- spec/classes/glance_api_spec.rb
+++ spec/classes/glance_api_spec.rb
@@ -438,7 +438,9 @@ describe 'glance::api' do
     end
     let(:params) { default_params }
 
-    it {should contain_package('glance-api')}
+    it { should contain_package('glance-api').with(
+        :tag => ['openstack'],
+    )}
   end
 
   describe 'on RedHat platforms' do
@@ -447,7 +449,9 @@ describe 'glance::api' do
     end
     let(:params) { default_params }
 
-    it { should contain_package('openstack-glance')}
+    it { should contain_package('openstack-glance').with(
+        :tag => ['openstack'],
+    )}
   end
 
   describe 'on unknown platforms' do
