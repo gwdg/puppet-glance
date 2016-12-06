ceff8656e42b34cdc2945776f24730a4bade1583
Emilien Macchi <emilien@redhat.com>
2016-12-06 16:50:37 +0100

    Allow to choose which Glance API to enable
    - Allow to configure Glance API v1 with enable_v1_api boolean.
      If set to True, the glance::registry::enable_v1_registry also needs to
      be set to True. Note that if you set these values to True, Glance will
      send some deprecation warnings.
      This option is separated from enable_v2_api, and both API versions can be
      deployed together.
    
    - Allow to configure Glance API v2 with enable_v2_api boolean.
      This option is separated from enable_v1_api, and both API versions can be
      deployed together.
    
    Change-Id: Ibbb8b970971c899ae94097042465f605fe55f748

--- manifests/registry.pp
+++ manifests/registry.pp
@@ -166,6 +166,14 @@
 #    Deprecated and will be replaced by ::glance::registry::authtoken::token_cache_time
 #    Defaults to undef.
 #
+#  [*enable_v1_registry*]
+#    (optional) Deploy the v1 API Registry service.
+#    If glance::api::enable_v1_api is set to True, this option also needs to be
+#    set to True.
+#    If your OpenStack deployment is only using Glance API v2, this option
+#    should be set to False.
+#    Defaults to $::os_service_default.
+#
 class glance::registry(
   $package_ensure          = 'present',
   $debug                   = undef,
@@ -204,6 +212,7 @@ class glance::registry(
   $signing_dir             = undef,
   $memcached_servers       = undef,
   $token_cache_time        = undef,
+  $enable_v1_registry      = $::os_service_default,
 ) inherits glance {
 
   include ::glance::deps
@@ -266,6 +275,7 @@ class glance::registry(
     'DEFAULT/workers':                value => $workers;
     'DEFAULT/bind_host':              value => $bind_host;
     'DEFAULT/bind_port':              value => $bind_port;
+    'DEFAULT/enable_v1_registry':     value => $enable_v1_registry;
     'glance_store/os_region_name':    value => $os_region_name;
   }
 
